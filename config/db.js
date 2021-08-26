83c9f7798ce083403285aa3f0670ed16467c1df4
yianzaratin <48094223+yianzaratin@users.noreply.github.com>
2021-08-26 11:03:50 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV == "production"){
-    module.exports = { mongoURI: "mongodb://yian:deus2014@blogapp.mglem.mongodb.net/myFirstDatabase"}
+    module.exports = { mongoURI: "mongodb://yian:********@blogapp.mglem.mongodb.net/myFirstDatabase"}
 }else{
     module.exports = { mongoURI: "mongodb://localhost/blogapp"}
 }
